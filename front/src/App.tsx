65232d85b8e34842f30ad2a231d4a8de99756a8a
JAllegre <ju.allegre@gmail.com>
2024-02-29 11:51:36 +0100

    create project

---
+++ front/src/App.tsx
@@ -0,0 +1,16 @@
+import reactLogo from "./assets/react.svg";
+
+function App() {
+  return (
+    <>
+      <div className="flex flex-col justify-center items-center p-3">
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <h1>UNDER CONSTRUCTION</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
