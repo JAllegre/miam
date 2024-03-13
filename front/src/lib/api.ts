390113563c1bd4c8310bd30f276568175299090e
JAllegre <ju.allegre@gmail.com>
2024-03-13 09:54:23 +0100

    fix api path

--- front/src/lib/api.ts
+++ front/src/lib/api.ts
@@ -1,13 +1,14 @@
 import { GetRecipeResponse, GetRecipesResponse } from "@/types";
+const miamRecipesApiPrefix = "/api/miam/recipes";
 
 export async function getAllRecipes(): Promise<GetRecipesResponse> {
-  const response = await fetch("/api/miam/recipes");
+  const response = await fetch(miamRecipesApiPrefix);
   return response.json();
 }
 
 export async function getOneRecipe(
   recipeId: number
 ): Promise<GetRecipeResponse> {
-  const response = await fetch(`/api/miam/recipes/${recipeId}`);
+  const response = await fetch(`${miamRecipesApiPrefix}/${recipeId}`);
   return response.json();
 }
